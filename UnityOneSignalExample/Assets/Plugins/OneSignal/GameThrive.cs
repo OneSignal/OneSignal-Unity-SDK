0205e498380e5823f9b02536f8cee224bbe9e9df
Josh Kasten <Josh@onesignal.com>
2015-04-14 14:16:02 -0700

    Fixed AndroidManifest.xml example and Obsolete warning.

--- UnityOneSignalExample/Assets/Plugins/OneSignal/GameThrive.cs
+++ UnityOneSignalExample/Assets/Plugins/OneSignal/GameThrive.cs
@@ -1,5 +1,5 @@
 ﻿using System;
 
-[Obsolete("GameThrive is decorated and will be removed in future releases. Please rename GameThrive to OneSignal in your code and your AndroidManifest.xml if your app is for Android.")]
+[Obsolete("GameThive is now called OneSignal. Please rename places where you use GameThrive to OneSignal including your AndroidManifest.xml if your app is for Android.")]
 public class GameThrive : OneSignal {
 }
