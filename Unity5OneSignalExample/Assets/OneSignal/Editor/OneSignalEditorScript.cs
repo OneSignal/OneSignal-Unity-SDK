2f27efa66d2cc35699141de34b84553a470b3ae0
Josh Kasten <Josh@onesignal.com>
2017-08-21 15:57:24 -0700

    Updated native Android OneSignal SDK to 3.6.0
    * This adds support Android O and it's new Notification Categories.
    * Also updated to the Google Unity jar Resolver 1.2.44.0

--- Unity5OneSignalExample/Assets/OneSignal/Editor/OneSignalEditorScript.cs
+++ Unity5OneSignalExample/Assets/OneSignal/Editor/OneSignalEditorScript.cs
@@ -40,9 +40,12 @@ public class OneSignalEditorScriptAndroid : AssetPostprocessor {
    public static object svcSupport;
 
    private static readonly string PluginName = "OneSignal";
-   // If you run into conflicts OneSignal supports back to version 9.0.0.
-   private static readonly string PLAY_SERVICES_VERSION = "10.0.+";
-   private static readonly string ANDROID_SUPPORT_VERSION = "24.0.+";
+   
+   // Both are set to LATEST to ensure compability between other plugins.
+   // PLAY_SERVICES_VERSION   - Tested with versions 10.2.1 to 11.2.0
+   // ANDROID_SUPPORT_VERSION - Tested with versions 26.0.0 through 26.0.1
+   private static readonly string PLAY_SERVICES_VERSION = "+";
+   private static readonly string ANDROID_SUPPORT_VERSION = "+";
 
    static OneSignalEditorScriptAndroid() {
       createOneSignalAndroidManifest();
@@ -96,22 +99,22 @@ public class OneSignalEditorScriptAndroid : AssetPostprocessor {
          namedArgs: new Dictionary<string, object>() {
              {"packageIds", new string[] { "extra-android-m2repository" } }
          });
-
-      // Adds play-services-base, play-services-basement, play-services-iid, and support-v4 will be automaticly added.
-      // Also adds play-services-tasks but this isn't used by OneSignal, it just added as a depency from the above.
-
-
-      // Setting 8.3+ does not work with unity-jar-resolver-1.2.0 and GooglePlayGamesPlugin-0.9.34.
-      //   It creates conflicting aar files with mismatched version of 8.4 and 9.4
-      // svcSupport.DependOn("com.google.android.gms", "play-services-gcm", "8.3+");
-      // svcSupport.DependOn("com.google.android.gms", "play-services-location", "8.3+");
-      // play-services-base, play-services-basement, and support-v4 will be automaticly added.
-      // play-services-maps and play-services-measurement are not used by OneSignal
-      //    but are included as depencies from the other parts of play-services.
+         
+         
+      Google.VersionHandler.InvokeInstanceMethod(
+         svcSupport, "DependOn",
+         new object[] {
+            "com.android.support",
+            "support-v4",
+            ANDROID_SUPPORT_VERSION
+         },
+         namedArgs: new Dictionary<string, object>() {
+             {"packageIds", new string[] { "extra-android-m2repository" } }
+         });
    }
    
    // Copies `AndroidManifestTemplate.xml` to `AndroidManifest.xml`
-   //   then replace `${manifestApplicationId}` with current packagename in the Unity settings. 
+   //   then replace `${manifestApplicationId}` with current packagename in the Unity settings.
    private static void createOneSignalAndroidManifest() {
       string oneSignalConfigPath = "Assets/Plugins/Android/OneSignalConfig/";
       string manifestFullPath = oneSignalConfigPath + "AndroidManifest.xml";
@@ -125,7 +128,7 @@ public class OneSignalEditorScriptAndroid : AssetPostprocessor {
       #if UNITY_5_6_OR_NEWER
          body = body.Replace("${manifestApplicationId}", PlayerSettings.applicationIdentifier);
       #else
-         body = body.Replace("${manifestApplicationId}", PlayerSettings.bundleIdentifier);     
+         body = body.Replace("${manifestApplicationId}", PlayerSettings.bundleIdentifier);
       #endif
       using (var streamWriter = new StreamWriter(manifestFullPath, false)) {
          streamWriter.Write(body);
