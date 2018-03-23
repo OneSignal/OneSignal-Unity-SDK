10c2c3e61cfadfcf3bff1fb073144644e13004a9
Brad Hesse <nightsd01@gmail.com>
2018-03-23 14:02:14 -0700

    2.6.4 Release

---
+++ OneSignalExample/OSExample_iOS/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Brad Hesse on 3/23/18.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
