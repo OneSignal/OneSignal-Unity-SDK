8782660aba2f988339041664ba9fb2fc5b42b231
Josh Kasten <josh@onesignal.com>
2019-12-06 13:32:22 -0800

    Omit generated Packages/manifest.json from build
    * manifest.json with old versions can cause the build script to crash
      - Script move moves it out before building so Unity will generate a safe default.
    * Also removed manifest.json from repo, we want a new default clean one every time.

--- buildpackage.sh
+++ buildpackage.sh
@@ -18,40 +18,46 @@ icons_location=$project_path/Assets/AppIcons
 icons_temp_location=$(pwd)/onesignal_temp/tempAppIcons
 android_location=$project_path/Assets/Plugins/Android
 config_location=$android_location/OneSignalConfig
+package_manifest=$project_path/Packages/manifest.json
+temp_package_manifest=$temp_location/Packages/manifest.json
 
-if [ ! -d $temp_location ]; then 
-   mkdir $temp_location
-fi
+mkdir -p $temp_location
 
 # Removed generated Android manifest files
-if [[ -e "$config_location/AndroidManifest.xml" ]]; then
-   rm $config_location/AndroidManifest.xml
-fi
-if [[ -e "$config_location/AndroidManifest.xml.meta" ]]; then 
-   rm $config_location/AndroidManifest.xml.meta
-fi
+rm $config_location/AndroidManifest.xml 2> /dev/null 
+rm $config_location/AndroidManifest.xml.meta 2> /dev/null 
+
+# temp move out Packages/manifest.json
+# This prevents possible crashes due to old versions in this manifest
+mkdir -p $temp_location/Packages/
+mv $package_manifest $temp_package_manifest
 
 #temporarily remove Android AppIcons 
 mv $icons_location $icons_temp_location
 mv $icons_location.meta $icons_temp_location.meta
 
-#temporarily move some necessary files
+## START - Clean Android files
+# This removes any .aar files we don't want to bundle in our package
+
+# temporarily move some necessary files
 mv $config_location $temp_location/OneSignalConfig
 mv $config_location.meta $temp_location/OneSignalConfig.meta
 
-#get rid of a bunch of unnecessary files
+# get rid of a bunch of unnecessary files
 rm -r $android_location
 mkdir $android_location
 
-#put the config files back
+# put the config files back
 mv $temp_location/OneSignalConfig $config_location
 mv $temp_location/OneSignalConfig.meta $config_location.meta
+
+## END - Clean Android files
 
 # Create the .unitypackage
 echo "Creating unitypackage."
 # Setting standalone keeps AndroidManifest.xml from being regenerated
 #   buildTarget must be before exportPackage for this to work
-/Applications/Unity/Hub/Editor/2019.1.12f1/Unity.app/Contents/MacOS/Unity \
+/Applications/Unity/Hub/Editor/2019.2.14f1/Unity.app/Contents/MacOS/Unity \
    -batchMode \
    -buildTarget standalone \
    -projectPath $project_path \
@@ -71,6 +77,9 @@ fi
 #the .unitypackage file is created in the OneSignalExample directory
 #move it to the root of the repo
 mv $generated_path $final_path
+
+# move back Packages/manifest.json
+mv $temp_package_manifest $package_manifest
 
 #move the icons back to their original location
 mv $icons_temp_location $icons_location
