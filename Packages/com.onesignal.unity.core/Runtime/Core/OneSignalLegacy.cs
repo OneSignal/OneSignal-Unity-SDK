269eeeed1e207462eb5901e4fe11ab7a6e501128
Stanislav Osipov <lacost.st@gmail.com>
2020-06-03 12:31:00 +0300

    OneSignal deprecated warning removed.

--- Packages/com.onesignal.unity.core/Runtime/Core/OneSignalLegacy.cs
+++ Packages/com.onesignal.unity.core/Runtime/Core/OneSignalLegacy.cs
@@ -1,8 +1,5 @@
-using System;
-
 /// <summary>
 /// Class is used for backwards compatibility for those who have not updated to use the Com.Onesignal namespace
 /// and it will be removed in the 3.0.0 release.
 /// </summary>
-[Obsolete("Please use Com.OneSignal.OneSignal instead. The OneSignal class will be removed in the 3.0.0 release.")]
 public class OneSignal : Com.OneSignal.OneSignal { }
