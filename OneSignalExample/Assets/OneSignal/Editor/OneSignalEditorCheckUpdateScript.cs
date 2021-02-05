0f38e6f89927fc66993eaa1c6790c8d01d8dae06
Josh Kasten <josh@onesignal.com>
2021-02-04 22:42:12 -0800

    Moved rest of InitializeOnLoad into script init
    * Since we interduced a centralized class to do all InitializeOnLoad
    with OneSignalEditorScriptInit I moved OneSignalEditorCheckUpdateScript
    there as the last remining script that wasn't there.

--- OneSignalExample/Assets/OneSignal/Editor/OneSignalEditorCheckUpdateScript.cs
+++ OneSignalExample/Assets/OneSignal/Editor/OneSignalEditorCheckUpdateScript.cs
@@ -32,22 +32,14 @@ using UnityEngine.Networking;
 using OneSignalPush.MiniJSON;
 using System.Collections;
 
-#if !UNITY_CLOUD_BUILD && UNITY_EDITOR && UNITY_2017_1_OR_NEWER
-
-[InitializeOnLoad]
-public class OneSignalEditorCheckUpdateScript : AssetPostprocessor
+internal class OneSignalEditorCheckUpdateScript
 {
    //this key is used for the current unity session only
    public static string sessionState = "onesignal_checked_update";
 
    static OneSignalUpdateRequest request;
 
-   static OneSignalEditorCheckUpdateScript()
-   {
-      Request();
-   }
-
-   static void Request()
+   internal static void Request()
    {
       //if the SDK already checked for an update during this session, no need to do so again
       if (SessionState.GetBool(sessionState, false)) {
@@ -128,5 +120,3 @@ public class OneSignalUpdateRequest : MonoBehaviour
       yield break;
    }
 }
-
-#endif
