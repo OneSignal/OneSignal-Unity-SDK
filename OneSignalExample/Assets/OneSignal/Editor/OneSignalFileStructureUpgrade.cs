4f5090888539a5c3d5fb5101feb52bbf9d9cc4cc
Josh Kasten <josh@onesignal.com>
2021-02-04 21:42:32 -0800

    Rename OneSignalConfig -> OneSignalConfig.plugin
    * This rename was interduced in PR #335, however it didn't account
    for those who would be updating the SDK.
    * Unity does not support file moves / renames in their ".unitypackage"
    so unfortunately this needs to be handled by a custom editor script.
    * Added class top down:
       - OneSignalFileStructureUpgrade - This is were we define specifc
       file moves / renames or deletes in the future if needed.
       - OneSignalFileLocator - This does a safe look up with GUID first
       but falls back to file name searches in case the Unity Dev moves
       files around.
       - OSUnityEditorUtils - These are helps to keep the other two classes
       as clean as possile away from specific Unity implementation details.

---
+++ OneSignalExample/Assets/OneSignal/Editor/OneSignalFileStructureUpgrade.cs
@@ -0,0 +1,31 @@
+#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+// Add any files or folders that need to be rename, moved, or deleted in
+// as part of the process when updating this SDK.
+[InitializeOnLoad]
+public class OneSignalFileStructureUpgrade : AssetPostprocessor {
+
+   static OneSignalFileStructureUpgrade() {
+      UpgradeToSDKVersion2_13_3();
+   }
+
+   private static void UpgradeToSDKVersion2_13_3() {
+      #if UNITY_ANDROID
+      RenameAndroidOneSignalConfig();
+      #endif
+   }
+
+   // This renames the folder "OneSignalConfig" to "OneSignal.plugin".
+   // ".plugin" is Unity documented folder post-fix required for some plugins features.
+   //    - This is required to fix Unity 2020+ capability.
+   // This is done via a script since .unitypackage files do not support renaming
+   // and so this is required for those upgrading from an older version of the SDK.
+   private static void RenameAndroidOneSignalConfig() {
+      var path = OneSignalFileLocator.GetOneSignalConfigFolderNameWithPath();
+      OSUnityEditorUtils.AppendFileExtensionIfMissing(path, "plugin");
+   }
+}
+#endif
