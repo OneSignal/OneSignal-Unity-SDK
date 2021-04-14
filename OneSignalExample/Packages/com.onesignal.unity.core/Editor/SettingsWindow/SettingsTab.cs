9570f9a32d5bc8b27b7400d718faa07aeb7b46c4
Stanislav Osipov <lacost.st@gmail.com>
2021-04-15 02:03:56 +0300

    chore: Settings window application id help-box.

--- OneSignalExample/Packages/com.onesignal.unity.core/Editor/SettingsWindow/SettingsTab.cs
+++ OneSignalExample/Packages/com.onesignal.unity.core/Editor/SettingsWindow/SettingsTab.cs
@@ -11,6 +11,7 @@ namespace Com.OneSignal.Editor
             var settings = OneSignalSettings.Instance;
             using (new WindowBlockWithIndent(new GUIContent("Settings")))
             {
+                EditorGUILayout.HelpBox("When Application Id is provided here, you may skip an appID param in the OneSignalPush.StartInit method.", MessageType.Info);
                 settings.ApplicationId = EditorGUILayout.TextField("Application Id: ", settings.ApplicationId);
             }
         }
