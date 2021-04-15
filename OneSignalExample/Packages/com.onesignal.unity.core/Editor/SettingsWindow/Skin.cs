288e607f34e98ea9c5710df26479e499cee33a3b
Viktor Kharchenko <vkharchenko.dev@gmail.com>
2021-04-15 14:59:22 +0300

    fix: added onesignal's icon in settings window

--- OneSignalExample/Packages/com.onesignal.unity.core/Editor/SettingsWindow/Skin.cs
+++ OneSignalExample/Packages/com.onesignal.unity.core/Editor/SettingsWindow/Skin.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
@@ -6,21 +5,12 @@ namespace Com.OneSignal.Editor
 {
     static class Skin
     {
-        static readonly Dictionary<string, Texture2D> s_Icons = new Dictionary<string, Texture2D>();
-        public static Texture2D SettingsWindowIcon => GetTextureAtPath(IconsPath);
-
-        private static string IconsPath => $"Packages/{ScopeRegistriesConfig.OneSignalScope}/Editor/Icons/"
+        public static Texture2D SettingsWindowIcon => AssetDatabase.LoadAssetAtPath(IconsPath, typeof(Texture2D)) as Texture2D;
+        
+        private static string IconsPath => $"Packages/{ScopeRegistriesConfig.OneSignalScope}.core/Editor/Icons/"
                                            + (EditorGUIUtility.isProSkin
                                                ? "icon_pro.png"
                                                : "icon_default.png" );
 
-        private static Texture2D GetTextureAtPath(string path)
-        {
-            if (s_Icons.ContainsKey(path)) return s_Icons[path];
-
-            var tex = AssetDatabase.LoadAssetAtPath(path, typeof(Texture2D)) as Texture2D;
-            s_Icons.Add(path, tex);
-            return tex;
-        }
     }
 }
