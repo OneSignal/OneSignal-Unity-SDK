21555a67f4e1b125ad40be5dd65115236bb477ac
Adam Schlesinger <adam@onesignal.com>
2021-07-13 13:25:11 -0400

    Updated installer window ui and moved installation logic off of OnGui

--- OneSignalExample/Packages/com.onesignal.unity.core/Editor/Installer/OneSignalInstallerWindow.cs
+++ OneSignalExample/Packages/com.onesignal.unity.core/Editor/Installer/OneSignalInstallerWindow.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
-/// 
+/// Pop up window which displays any additional required or optional setup steps by the SDK
 /// </summary>
 public class OneSignalInstallerWindow : EditorWindow
 {
@@ -17,8 +18,16 @@ public class OneSignalInstallerWindow : EditorWindow
     }
 
     private const string _title = "OneSignal Component Installer";
-    private const string _header = "";
+    private const string _description = "Additional steps required to get the OneSignal Unity SDK up and running";
+    
     private IReadOnlyList<OneSignalInstallerStep> _installSteps;
+    private readonly Queue<Action> _actionsToPerform = new Queue<Action>();
+    
+    private bool _guiSetupComplete = false;
+    private GUIStyle _summaryStyle;
+    private GUIStyle _detailsStyle;
+    private Texture _checkTexture;
+    private Texture _boxTexture;
     
     private void OnEnable()
     {
@@ -38,29 +47,79 @@ public class OneSignalInstallerWindow : EditorWindow
 
     private void OnGUI()
     {
-        GUILayout.Label(_header, EditorStyles.boldLabel);
+        _setupGUI();
+
+        GUILayout.Label(_description);
         EditorGUILayout.Separator();
 
         if (_installSteps == null) 
             return;
+
+        if (GUILayout.Button("Run All Steps"))
+        {
+            foreach (var step in _installSteps)
+                _actionsToPerform.Enqueue(step.Install);
+        }
+        
+        EditorGUILayout.Separator();
         
         foreach (var step in _installSteps)
         {
             EditorGUILayout.BeginHorizontal();
-            
-            GUILayout.Label(step.Summary);
-            
-            var buttonText = step.IsStepCompleted ? "Reinstall" : "Install";
-            if (GUILayout.Button(buttonText))
-                step.Install();
+
+            var sumContent = new GUIContent(step.Summary);
+            var sumRect = GUILayoutUtility.GetRect(sumContent, _summaryStyle);
+
+            var checkRect = new Rect(sumRect.x, sumRect.y, sumRect.height, sumRect.height);
+            GUI.DrawTexture(checkRect, step.IsStepCompleted ? _checkTexture: _boxTexture);
+
+            sumRect.x += sumRect.height + EditorStyles.label.padding.left;
+            GUI.Label(sumRect, sumContent);
+
+            EditorGUI.BeginDisabledGroup(step.IsStepCompleted);
+            _handleButtonResult(GUILayout.Button("Install"), step.Install);
+            EditorGUI.EndDisabledGroup();
             
             EditorGUILayout.EndHorizontal();
 
-            GUILayout.Label(step.Details);
+            GUILayout.Label(step.Details, _detailsStyle);
             EditorGUILayout.Separator();
         }
     }
-    
+
+    private void OnInspectorUpdate()
+    {
+        while (_actionsToPerform.Count > 0)
+            _actionsToPerform.Dequeue().Invoke();
+    }
+
+    private void _setupGUI()
+    {
+        if (_guiSetupComplete)
+            return;
+
+        _summaryStyle = EditorStyles.boldLabel;
+        
+        _detailsStyle = new GUIStyle(GUI.skin.textField)
+        {
+            wordWrap = true
+        };
+
+        var checkContent = EditorGUIUtility.IconContent("TestPassed");
+        _checkTexture = checkContent.image;
+
+        var boxContent = EditorGUIUtility.IconContent("Warning");
+        _boxTexture = boxContent.image;
+        
+        _guiSetupComplete = true;
+    }
+
+    private void _handleButtonResult(bool buttonResult, Action buttonAction)
+    {
+        if (buttonResult && !_actionsToPerform.Contains(buttonAction))
+            _actionsToPerform.Enqueue(buttonAction);
+    }
+
     private static IEnumerable<Type> _findAllAssignableTypes<T>(string assemblyFilter)
     {
         var assignableType = typeof(T);
