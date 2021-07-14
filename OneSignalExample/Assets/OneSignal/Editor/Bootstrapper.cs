5b166f9de4c620316e1631ae079f0488fd3ada0d
Adam Schlesinger <adam@onesignal.com>
2021-07-14 11:30:59 -0400

    Bootstrapper now only checks for legacy and asks to open installer

--- OneSignalExample/Assets/OneSignal/Editor/Bootstrapper.cs
+++ OneSignalExample/Assets/OneSignal/Editor/Bootstrapper.cs
@@ -1,125 +1,42 @@
 ﻿using System.Linq;
 using UnityEditor;
-using UnityEngine;
 
-    [InitializeOnLoad]
-    static class Bootstrapper
+/// <summary>
+/// Handles informing the user on startup/import if the legacy SDK has been detected
+/// </summary>
+public static class Bootstrapper
+{
+    /// <summary>
+    /// Asks to open the installer if legacy files are found
+    /// </summary>
+    [InitializeOnLoadMethod]
+    public static void CheckForLegacy()
     {
-        static Bootstrapper()
-        {
-            if (!AssetDatabase.FindAssets("onesignal-bootstrap-lock", new[] {"Assets"}).Any())
-                InstallLatestOneSignalRelease();
-            else
-                Debug.LogWarning("'lock' file found. Bootstrap execution locked.");
-        }
+        if (SessionState.GetBool(_sessionCheckKey, false))
+            return;
+        
+        SessionState.SetBool(_sessionCheckKey, true);
+        
+        var inventory = AssetDatabase.LoadAssetAtPath<OneSignalFileInventory>(OneSignalFileInventory.AssetPath);
 
-        static bool IsOneSignalCoreInstalled
-        {
-            get
-            {
-#if ONE_SIGNAL_INSTALLED
-                return true;
-#else
-                return false;
-#endif
-            }
-        }
+        if (inventory == null)
+            return; // error
+        
+        var diff = inventory.CurrentPaths.Except(inventory.DistributedPaths);
 
-        internal static void InstallLatestOneSignalRelease()
-        {
-            if (IsOneSignalCoreInstalled)
-            {
-                EditorApplication.delayCall += () =>
-                {
-                    EditorUtility.DisplayDialog("Successes",
-                        "OneSignal installation completed. Thank you!",
-                        "Ok");
-                    UninstallBootstrapper();
-                };
-                return;
-            }
+        if (!diff.Any())
+            return;
+        
+        var dialogResult = EditorUtility.DisplayDialog(
+            "OneSignal",
+            "The project contains an outdated version of OneSignal SDK! We recommend running the OneSignal installer.",
+            "Open Installer",
+            "Cancel"
+        );
 
-            if (FindRemainingDirectoriesOfOutdatedSDK(out var directories))
-            {
-                if (EditorUtility.DisplayDialog("OneSignal",
-                    "The project contains an outdated version of OneSignal SDK! It has to be removed in order to continue the installation.",
-                    "Remove and continue",
-                    "Cancel installation"))
-                {
-                    CleanUpUtility.RemoveDirectories(directories);
-                }
-                else
-                {
-                    EditorApplication.delayCall += UninstallBootstrapper;
-                    return;
-                }
-            }
-            else
-            {
-                EditorUtility.DisplayDialog("OneSignal",
-                    "Installation started. Thank you!",
-                    "Ok");
-            }
-
-            GitHubUtility.GetLatestRelease(BootstrapperConfig.GitHubRepositoryURL, Bootstrap);
-        }
-
-        static void UninstallBootstrapper()
-        {
-            UnityEditor.PackageManager.Client.Remove(BootstrapperConfig.BootstrapperPackageName);
-            CleanUpUtility.RemoveDirectories(BootstrapperConfig.BootstrapperFolderPath);
-        }
-
-        static bool FindRemainingDirectoriesOfOutdatedSDK(out string[] directories)
-        {
-            directories = BootstrapperConfig.OutdatedSDKDirectories
-                .Where(AssetDatabase.IsValidFolder)
-                .ToArray();
-            return directories.Any();
-        }
-
-        static void Bootstrap(string latestRelease)
-        {
-            var manifest = new Manifest();
-            manifest.Fetch();
-
-            var manifestUpdated = false;
-
-            if (!manifest.IsRegistryPresent(BootstrapperConfig.NpmjsScopeRegistryUrl))
-            {
-                manifest.AddScopeRegistry(BootstrapperConfig.NpmjsScopeRegistry);
-                manifestUpdated = true;
-            }
-            else
-            {
-                var npmjsScopeRegistry = manifest.GetScopeRegistry(BootstrapperConfig.NpmjsScopeRegistryUrl);
-                if (!npmjsScopeRegistry.HasScope(BootstrapperConfig.OneSignalScope))
-                {
-                    npmjsScopeRegistry.AddScope(BootstrapperConfig.OneSignalScope);
-                    manifestUpdated = true;
-                }
-            }
-
-            // UnityEditor.PackageManager.Client.Add method doesn't work in Unity versions older then 2019.
-            // Thus, we need to manually add dependencies.
-            // Probably we need to make something similar to OneSignalUpdateRequest to get the latest package version.
-
-            if (!manifest.IsDependencyPresent(BootstrapperConfig.OneSignalAndroidName))
-            {
-                manifest.AddDependency(BootstrapperConfig.OneSignalAndroidName, latestRelease);
-                manifestUpdated = true;
-            }
-
-            if (!manifest.IsDependencyPresent(BootstrapperConfig.OneSignalIOSName))
-            {
-                manifest.AddDependency(BootstrapperConfig.OneSignalIOSName, latestRelease);
-                manifestUpdated = true;
-            }
-
-            if (manifestUpdated)
-            {
-                manifest.ApplyChanges();
-                AssetDatabase.Refresh();
-            }
-        }
+        if (dialogResult) 
+            OneSignalInstallerWindow.ShowWindow();
     }
+
+    private const string _sessionCheckKey = "onesignal.bootstrapper.check";
+}
