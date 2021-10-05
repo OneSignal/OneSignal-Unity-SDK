971c99307986c39ca493c8eae7d3552c7bda520a
Adam Schlesinger <adam.d.schlesinger@gmail.com>
2021-10-05 12:05:24 -0400

    WIP on example scene and behaviour

--- com.onesignal.unity.core/Runtime/AssemblyInfo.cs
+++ com.onesignal.unity.core/Runtime/AssemblyInfo.cs
@@ -3,4 +3,8 @@
 [assembly: InternalsVisibleTo("OneSignal.iOS")]
 [assembly: InternalsVisibleTo("OneSignal.Android")]
 [assembly: InternalsVisibleTo("OneSignal.Core.Editor")]
+<<<<<<< main
 [assembly: InternalsVisibleTo("OneSignal.UnityPackage.Attribution")]
+=======
+[assembly: InternalsVisibleTo("OneSignal.UnityPackage.Example")]
+>>>>>>> WIP on example scene and behaviour
