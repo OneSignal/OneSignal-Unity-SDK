3c427e438495b2f66034ebdc1cd957a9e453a1fc
Adam Schlesinger <adam.d.schlesinger@gmail.com>
2022-01-14 14:07:15 -0500

    [MAINTENANCE] Change edm4u step to use new installer

--- com.onesignal.unity.android/Editor/SetupSteps/InstallEdm4uStep.cs
+++ com.onesignal.unity.android/Editor/SetupSteps/InstallEdm4uStep.cs
@@ -1,8 +1,5 @@
-using System.IO;
 using System.Linq;
-using UnityEditor;
 using UnityEditor.Compilation;
-using UnityEngine;
 
 /// <summary>
 /// Checks for EDM4U assemblies and installs the package from its github releases
@@ -23,27 +20,14 @@ public sealed class InstallEdm4uStep : OneSignalSetupStep {
            .Any(assemblyName => assemblyName.StartsWith("Google.VersionHandler"));
 
     protected override void _runStep() {
-        var request = EditorWebRequest.Get(_edm4UPackageDownloadUrl);
-        request.AddEditorProgressDialog("Downloading Google External Dependency Manager");
-        request.Send(unityRequest => {
-            if (unityRequest.error != null) {
-                EditorUtility.DisplayDialog("Package Download failed.", unityRequest.error, "Ok");
-                return;
-            }
-
-            //Asset folder name remove
-            var projectPath = Application.dataPath.Substring(0, Application.dataPath.Length - 6);
-            var tmpPackageFile = projectPath + FileUtil.GetUniqueTempPathInProject() + ".unityPackage";
-
-            File.WriteAllBytes(tmpPackageFile, unityRequest.downloadHandler.data);
-            AssetDatabase.ImportPackage(tmpPackageFile, false);
-            
-            _shouldCheckForCompletion = true;
+        const string msg = "Downloading Google External Dependency Manager";
+        UnityPackageInstaller.DownloadAndInstall(_edm4UPackageDownloadUrl, msg, result => {
+            if (result)
+                _shouldCheckForCompletion = true;
         });
     }
 
     private const string _edm4UVersion = "1.2.167";
-
-    static readonly string _edm4UPackageDownloadUrl
+    private static readonly string _edm4UPackageDownloadUrl
         = $"https://github.com/googlesamples/unity-jar-resolver/blob/v{_edm4UVersion}/external-dependency-manager-{_edm4UVersion}.unitypackage?raw=true";
 }
