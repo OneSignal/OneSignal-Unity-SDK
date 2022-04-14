2f13ee95ac48c72a92dbb25ed765034983c76848
Adam Schlesinger <adam.d.schlesinger@gmail.com>
2022-04-14 13:38:29 -0400

    Add docs for iam action

--- com.onesignal.unity.core/Runtime/Models/InAppMessageAction.cs
+++ com.onesignal.unity.core/Runtime/Models/InAppMessageAction.cs
@@ -29,10 +29,28 @@ using System;
 using UnityEngine;
 
 namespace OneSignalSDK {
+    /// <summary>
+    /// Action associated with clicking a button in an In-App Message
+    /// </summary>
     [Serializable] public sealed class InAppMessageAction : ISerializationCallbackReceiver {
+        /// <summary>
+        /// An optional click name defined for the action element
+        /// </summary>
         public string clickName;
+
+        /// <summary>
+        /// An optional URL that opens when the action takes place
+        /// </summary>
         public string clickUrl;
+
+        /// <summary>
+        /// Whether this is the first time the user has clicked any action on the In-App Message
+        /// </summary>
         public bool firstClick;
+
+        /// <summary>
+        /// Whether this action will close the In-App Message
+        /// </summary>
         public bool closesMessage;
 
     #region Native Field Handling
