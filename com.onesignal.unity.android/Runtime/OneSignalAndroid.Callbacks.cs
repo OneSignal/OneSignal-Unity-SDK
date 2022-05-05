19d1b4dc64b9634a8949dc8d650890fbc9772ebe
Shepherd <andrealee053@gmail.com>
2022-05-05 15:27:57 -0400

    Add callback to setLanguage

--- com.onesignal.unity.android/Runtime/OneSignalAndroid.Callbacks.cs
+++ com.onesignal.unity.android/Runtime/OneSignalAndroid.Callbacks.cs
@@ -254,6 +254,19 @@ namespace OneSignalSDK {
             }
         }
 
+        private sealed class OSSetLanguageCompletionHandler : OneSignalAwaitableAndroidJavaProxy<bool> {
+            public OSSetLanguageCompletionHandler() : base("OSSetLanguageCompletionHandler") { }
+
+            /// <param name="results">string</param>
+            public void onSuccess(AndroidJavaObject results) => _complete(true);
+
+            /// <param name="error">OSLanguageError</param>
+            public void onFailure(AndroidJavaObject error) {
+                _handleError(error);
+                _complete(false);
+            }
+        }
+
         private sealed class OutcomeCallback : OneSignalAwaitableAndroidJavaProxy<bool> {
             public OutcomeCallback() : base("OutcomeCallback") { }
 
