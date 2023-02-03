402112cf1b9d290f79dddc6c6928945cca63b891
Shepherd <andrealee053@gmail.com>
2023-02-03 17:34:29 -0500

    Update iOS api

--- com.onesignal.unity.ios/Runtime/iOSDebugManager.cs
+++ com.onesignal.unity.ios/Runtime/iOSDebugManager.cs
@@ -33,7 +33,7 @@ using OneSignalSDK.Debug.Models;
 namespace OneSignalSDK.iOS.Debug {
     internal sealed class iOSDebugManager : IDebugManager {
         [DllImport("__Internal")] private static extern void _debugSetLogLevel(int logLevel);
-        [DllImport("__Internal")] private static extern void _debugSetVisualLevel(int visualLevel);
+        [DllImport("__Internal")] private static extern void _debugSetAlertLevel(int alertlLevel);
 
         private LogLevel _logLevel = LogLevel.Warn;
         private LogLevel _alertLevel = LogLevel.None;
@@ -50,7 +50,7 @@ namespace OneSignalSDK.iOS.Debug {
             get => _alertLevel;
             set {
                 _alertLevel = value;
-                _debugSetVisualLevel((int) value);
+                _debugSetAlertLevel((int) value);
             }
         }
     }
