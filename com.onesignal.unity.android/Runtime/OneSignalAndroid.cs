3a947d34b638d0fd5c9623b1397a01ea358e3d2b
Shepherd <andrealee053@gmail.com>
2023-02-03 11:40:42 -0500

    Update Android api

--- com.onesignal.unity.android/Runtime/OneSignalAndroid.cs
+++ com.onesignal.unity.android/Runtime/OneSignalAndroid.cs
@@ -51,6 +51,8 @@ namespace OneSignalSDK.Android {
 
         private readonly AndroidJavaClass _sdkClass = new AndroidJavaClass(QualifiedSDKClass);
 
+        private readonly AndroidJavaClass _sdkWrapperClass = new AndroidJavaClass(SDKPackage + ".common.OneSignalWrapper");
+
         private static OneSignalAndroid _instance;
 
         private AndroidUserManager _user;
@@ -111,6 +113,9 @@ namespace OneSignalSDK.Android {
         public override void Initialize(string appId) {
             var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
             var activity    = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+            _sdkWrapperClass.CallStatic("setSdkType", "unity");
+            //_sdkWrapperClass.CallStatic("setSdkVersion", "050000");
 
             _sdkClass.CallStatic("initWithContext", activity, appId);
 
