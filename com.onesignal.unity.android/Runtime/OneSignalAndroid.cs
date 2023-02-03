684d650d4a30872429f040780fc0321065b6f455
Shepherd <andrealee053@gmail.com>
2023-02-03 11:40:05 -0500

    Add support for login with jwt token

--- com.onesignal.unity.android/Runtime/OneSignalAndroid.cs
+++ com.onesignal.unity.android/Runtime/OneSignalAndroid.cs
@@ -141,7 +141,7 @@ namespace OneSignalSDK.Android {
         }
 
         public override void Login(string externalId, string jwtBearerToken = null) {
-            _sdkClass.CallStatic("login", externalId);
+            _sdkClass.CallStatic("login", externalId, jwtBearerToken);
         }
 
         public override void Logout() {
