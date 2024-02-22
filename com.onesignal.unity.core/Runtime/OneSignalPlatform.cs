53d2cb666fbc05d38c70fba531e5066d2a0b32d6
jinliu9508 <jinliu9508@gmail.com>
2024-02-22 16:34:17 -0500

    release 5.1.0

--- com.onesignal.unity.core/Runtime/OneSignalPlatform.cs
+++ com.onesignal.unity.core/Runtime/OneSignalPlatform.cs
@@ -38,7 +38,7 @@ using OneSignalSDK.LiveActivities;
 
 namespace OneSignalSDK {
     public abstract class OneSignalPlatform {
-        public const string VersionHeader = "050006";
+        public const string VersionHeader = "050100";
 
         internal static event Action<string> OnInitialize;
 
