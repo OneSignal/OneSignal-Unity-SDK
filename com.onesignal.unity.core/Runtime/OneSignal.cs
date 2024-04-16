0d240083aef2651f985f273086a03f685e8b59b5
Shepherd <andrealee053@gmail.com>
2024-04-16 10:13:23 -0400

    Bump version numbers

--- com.onesignal.unity.core/Runtime/OneSignal.cs
+++ com.onesignal.unity.core/Runtime/OneSignal.cs
@@ -40,7 +40,7 @@ namespace OneSignalSDK {
     /// OneSignal SDK for Unity
     /// </summary>
     public static partial class OneSignal {
-        public const string Version = "5.1.2";
+        public const string Version = "5.1.3";
 
         /// <summary>
         /// The default static instance of the OneSignal Unity SDK
