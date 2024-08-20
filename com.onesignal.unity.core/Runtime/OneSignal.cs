2e9d13cef3e562c1bfb33e8b6ceba91191818595
Shepherd <andrealee053@gmail.com>
2024-08-20 16:15:05 -0400

    Bump version numbers to 5.1.8

--- com.onesignal.unity.core/Runtime/OneSignal.cs
+++ com.onesignal.unity.core/Runtime/OneSignal.cs
@@ -40,7 +40,7 @@ namespace OneSignalSDK {
     /// OneSignal SDK for Unity
     /// </summary>
     public static partial class OneSignal {
-        public const string Version = "5.1.7";
+        public const string Version = "5.1.8";
 
         /// <summary>
         /// The default static instance of the OneSignal Unity SDK
--- com.onesignal.unity.core/Runtime/OneSignalPlatform.cs
+++ com.onesignal.unity.core/Runtime/OneSignalPlatform.cs
@@ -38,7 +38,7 @@ using OneSignalSDK.LiveActivities;
 
 namespace OneSignalSDK {
     public abstract class OneSignalPlatform {
-        public const string VersionHeader = "050107";
+        public const string VersionHeader = "050108";
 
         internal static event Action<string> OnInitialize;
 
