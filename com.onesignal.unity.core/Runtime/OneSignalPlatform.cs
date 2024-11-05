1951e740056023fda73675612b40d2207a26514f
Shepherd <andrealee053@gmail.com>
2024-11-05 17:13:58 -0500

    Bump version numbers to 5.1.10

--- com.onesignal.unity.core/Runtime/OneSignalPlatform.cs
+++ com.onesignal.unity.core/Runtime/OneSignalPlatform.cs
@@ -38,7 +38,7 @@ using OneSignalSDK.LiveActivities;
 
 namespace OneSignalSDK {
     public abstract class OneSignalPlatform {
-        public const string VersionHeader = "050109";
+        public const string VersionHeader = "050110";
 
         internal static event Action<string> OnInitialize;
 
