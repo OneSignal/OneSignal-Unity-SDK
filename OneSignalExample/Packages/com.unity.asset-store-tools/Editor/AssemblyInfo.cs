c0ae9373a0893d7dcf334ba571f7b1b7f19cddea
Fadi George <fadii925@gmail.com>
2026-01-28 11:55:41 -0800

    remove asset store tools

--- OneSignalExample/Packages/com.unity.asset-store-tools/Editor/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("Unity.AssetStoreTools.Editor.Tests.asmdef")]
-[assembly: InternalsVisibleTo("ab-builder")]
-[assembly: InternalsVisibleTo("Inspector-Editor")]
