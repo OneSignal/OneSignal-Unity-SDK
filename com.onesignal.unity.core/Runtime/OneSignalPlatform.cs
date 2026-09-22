49ab0b159ed3010f586012a117d023a65ce27a32
Nan <nan@onesignal.com>
2026-09-22 16:21:48 -0700

    Bump the release version to 5.4.0
    The pipeline bumps the patch version by default. Raising the iOS deployment target from 12.0 to 15.0 drops apps that target iOS 12 to 14, so this release ships as a minor.

--- com.onesignal.unity.core/Runtime/OneSignalPlatform.cs
+++ com.onesignal.unity.core/Runtime/OneSignalPlatform.cs
@@ -40,7 +40,7 @@ namespace OneSignalSDK
 {
     public abstract class OneSignalPlatform
     {
-        public const string VersionHeader = "050307";
+        public const string VersionHeader = "050400";
 
         internal static event Action<string> OnInitialize;
 
